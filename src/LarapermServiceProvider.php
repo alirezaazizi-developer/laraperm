989d7767194d1c1bc9b26b87493beb7eacf00d61
Alireza Azizi <dev.azizimail@gmail.com>
2020-11-17 14:49:28 +0330

    create service provider and some package feature

---
+++ src/LarapermServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Azizi\Laraperm;
+
+use Illuminate\Support\ServiceProvider;
+
+class LarapermServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        dd('sucess');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ src/Traits/LaraparamTrait.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Azizi\Larapram\Traits;
+
+trait LarapramTrait{
+
+    public function owner() :bool
+    {
+        return true;
+    }
+}
